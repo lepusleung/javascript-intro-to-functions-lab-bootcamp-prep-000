13ce09eac1fd15b066c513df47ed2402f8cdfe88
Man Leung <lepusleung@gmail.com>
2017-09-19 19:30:36 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -17,22 +17,14 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return 'I can\'t hear you!''
-  } else if 
+    return "I can\'t hear you!'"
+  } else if
   (string === string.uppercase()) {
-    
-  }
-  )
+    return "YES INDEED!"
+  } else if 
+  (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
 
 
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
